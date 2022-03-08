74bcc5f06fe7ee5f1dfbb5856e1495b5da5585d7
Sakel4 <sakellarissfakiotakis@outlook.com>
2022-03-08 21:47:27 +0200

    initial commit

---
+++ app/src/main/java/com/example/listproject/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.listproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.listView = findViewById(R.id.listView);
+        this.spinner = findViewById(R.id.spinner);
+
+//        final ArrayList<String> students = new ArrayList<>();
+//        students.add("Bob");
+//        students.add("John");
+//        students.add("Marques");
+//        students.add("Linus");
+//        students.add("Elon");
+//
+//        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, students);
+//        spinner.setAdapter(studentsAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You have selected " + spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(MainActivity.this, "Nothing has been selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final ArrayList<String> cities = new ArrayList<>();
+        cities.add("Zurich");
+        cities.add("New York");
+        cities.add("Berlin");
+        cities.add("Moscow");
+        cities.add("Madrid");
+
+        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
+        listView.setAdapter(citiesAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, cities.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
